2b9a5b6ca78a8748c142cb8b0875195a4ee04bda
Arafat Al Mahmud <arafat@Arafats-MacBook-Pro.local>
2023-05-14 17:55:47 +0600

    Refactor min heap

--- heap/min-heap.ts
+++ heap/min-heap.ts
@@ -8,7 +8,6 @@ export class MinHeap<T> {
 
   insert(item: Item<T>) {
     this.heap.push(item);
-
     let newIndex = this.heap.length - 1;
 
     while (newIndex > 0) {
@@ -27,48 +26,29 @@ export class MinHeap<T> {
   }
 
   delete(): Item<T> | undefined {
-    if (this.heap.length > 0) {
-      let currentIndex = 0;
-      const item = this.heap[currentIndex];
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const lastItem = this.heap.pop();
-
-      if (lastItem) {
-        this.heap[currentIndex] = lastItem;
-      }
-
-      while (this.heap[this.leftChildIndex(currentIndex)]) {
-        const leftChild = this.heap[this.leftChildIndex(currentIndex)];
-        const rightChild = this.heap[this.rightChildIndex(currentIndex)];
-
-        let swapIndex = -1;
-
-        const currentItem = this.heap[currentIndex];
-
-        if (
-          currentItem.value > leftChild.value ||
-          currentItem.value > rightChild.value
-        ) {
-          if (leftChild.value < rightChild.value) {
-            swapIndex = this.leftChildIndex(currentIndex);
-          } else {
-            swapIndex = this.rightChildIndex(currentIndex);
-          }
-        }
-
-        if (swapIndex > -1) {
-          [this.heap[currentIndex], this.heap[swapIndex]] = [
-            this.heap[swapIndex],
-            this.heap[currentIndex],
-          ];
-          currentIndex = swapIndex;
-        } else {
-          break;
-        }
-      }
-
-      return item;
+    if (this.heap.length === 1) {
+      return this.heap.pop();
     }
+
+    const item = this.heap[0];
+    this.heap[0] = this.heap.pop() as Item<T>;
+    let currentIndex = 0;
+
+    while (this.heap[this.leftChildIndex(currentIndex)]) {
+      const smallerChildIndex = this.determineSmallerChildIndex(currentIndex);
+
+      if (smallerChildIndex > -1) {
+        [this.heap[currentIndex], this.heap[smallerChildIndex]] = [
+          this.heap[smallerChildIndex],
+          this.heap[currentIndex],
+        ];
+        currentIndex = smallerChildIndex;
+      } else {
+        break;
+      }
+    }
+
+    return item;
   }
 
   leftChildIndex(index: number) {
@@ -83,7 +63,33 @@ export class MinHeap<T> {
     return Math.floor((index - 1) / 2);
   }
 
+  determineSmallerChildIndex(index: number): number {
+    const currentItem = this.heap[index];
+    const leftChild = this.heap[this.leftChildIndex(index)];
+    const rightChild = this.heap[this.rightChildIndex(index)];
+    let smallerChildIndex = -1;
+
+    if (
+      (leftChild && currentItem.value > leftChild.value) ||
+      (rightChild && currentItem.value > rightChild.value)
+    ) {
+      if (!rightChild) {
+        smallerChildIndex = this.leftChildIndex(index);
+      } else if (leftChild.value < rightChild.value) {
+        smallerChildIndex = this.leftChildIndex(index);
+      } else {
+        smallerChildIndex = this.rightChildIndex(index);
+      }
+    }
+
+    return smallerChildIndex;
+  }
+
   getItems(): Item<T>[] {
     return this.heap;
+  }
+
+  size(): number {
+    return this.heap.length;
   }
 }
